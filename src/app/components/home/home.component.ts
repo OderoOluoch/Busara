32d2d87d9d61acdfe81df14500948931e1e8f475
OderoOluoch <oderoluoch@gmail.com>
2021-07-22 15:29:36 +0300

    Putting it all together- for the full funtionality

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  userDetails: any;
 
-  constructor() { }
+ constructor(private userService: UserService) { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+  this.userService.getLogedInUser().subscribe(
+    (data : any) => {
+      this.userDetails = data;
+      // console.log(data)
+    }
+  )
+}
 
 }
