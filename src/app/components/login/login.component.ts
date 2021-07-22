0e256146732c44f59b68674b89b5d4c26f540184
OderoOluoch <oderoluoch@gmail.com>
2021-07-22 14:53:34 +0300

    Set up the function on the user login comonent that runs the login process

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/models/user.class';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = new User;
+  isLoginError : boolean = false;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router :Router
+  ) { }
 
   ngOnInit(): void {
+    this.resetForm();
+  }
+
+  resetForm(form? :NgForm){
+    if(form != null )
+    form.reset();
+    this.user = {
+      username:'',
+      email: '',
+      password1:'',
+      password2:'',
+      referral_code:'',
+      phone_number:'',
+      full_name:'',
+      device_details: '',
+      location: ''
+    }
+  }
+
+  login(username:string,password:string){
+    this.userService.userAuthentication(username,password)
+      .subscribe((data:any) =>{
+          localStorage.setItem('userToken', data.access_token);
+          this.router.navigate(['']);
+          console.log(data);
+      },
+      (err :HttpErrorResponse) => {
+          this.isLoginError = true;
+          console.log(err);
+      })
+
   }
 
 }
