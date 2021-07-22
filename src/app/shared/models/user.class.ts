66984edbb45f0eb9935638a5bac3cf22d0541879
OderoOluoch <oderoluoch@gmail.com>
2021-07-22 14:40:07 +0300

    populated the user class and set all values to nullable.

--- src/app/shared/models/user.class.ts
+++ src/app/shared/models/user.class.ts
@@ -1,2 +1,11 @@
 export class User {
+    username?: String;
+    email?: String;
+    password1?: String;
+    password2?: String;
+    referral_code?: String;
+    phone_number?: String;
+    full_name?: String;
+    device_details?: String;
+    location?: String;
 }
