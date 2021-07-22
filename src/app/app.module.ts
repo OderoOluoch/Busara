2aae17aa9a9162b3031d20f155ffaa3e633d658c
OderoOluoch <oderoluoch@gmail.com>
2021-07-22 14:29:31 +0300

    for the fun of it, we have the register module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { SurveyComponent } from './components/survey/survey.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SurveyComponent } from './components/survey/survey.component';
     LoginComponent,
     NavComponent,
     HomeComponent,
-    SurveyComponent
+    SurveyComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
